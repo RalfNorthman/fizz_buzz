17b28019c880c02861ca6e6bd567ed9e3d367477
RalfNorthman <ralf.northman@bredband.net>
2017-05-14 18:25:32 +0200

    Initial commit.

---
+++ fizz_buzz.ex
@@ -0,0 +1,33 @@
+defmodule FizzBuzz do
+
+  def case_fizzbuzz(n) do
+    func = fn i -> 
+      case [rem(i, 3), rem(i, 5), i] do
+        [0, 0, _] -> "FizzBuzz"
+        [0, _, _] -> "Fizz"
+        [_, 0, _] -> "Buzz"
+        [_, _, i] -> i
+      end
+    end
+    import Enum
+    1..n |> map(func) |> each(&IO.puts/1) 
+  end
+
+  def stream_fizzbuzz(n) do
+    import Stream
+    nums   = iterate 1, &(&1 + 1)
+    fizzes = cycle ["", "", "Fizz"]
+    buzzes = cycle ["", "", "", "", "Buzz"]
+    func   = fn { {"", ""}, number } -> number
+                { {fi, bu}, _num } -> fi <> bu 
+    end
+    zip(fizzes, buzzes) 
+    |> zip(nums)
+    |> map(func)
+    |> take(n)
+    |> Enum.each(&IO.puts/1)
+  end
+
+end
+
+Mod.stream_fizzbuzz(100)
